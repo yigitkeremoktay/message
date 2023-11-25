9a70f34ad6c7e1567d2f288e1e0c3869b1ce5e7d
Yigit Kerem Oktay <yigitkerem@theskyfallen.com>
2023-11-26 00:30:48 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Yay, see you on the 18th!";
+  question.innerHTML = "jk, i don't have any dates.";
   gif.src =
     "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
 });
